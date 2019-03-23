b48fda9b18537729be1a6549b71fbb3e8bbb5cc0
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2019-03-23 15:58:25 +0900

    add starter code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Shopping from './Shopping';
+import Game from './Game';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Shopping />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
